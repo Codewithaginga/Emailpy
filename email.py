987e3b743e20bbc40d220b1152854bc174e560f9
aginga <brianaginga35@gmail.com>
2022-09-19 12:56:56 +0300

    new code added

--- email.py
+++ email.py
@@ -1,1 +1,24 @@
 print('check your email')
+class Currency:
+
+    @staticmethod
+    def pound_to_shillings():
+
+        dollar = int(input('Enter amount: '))
+
+        kenya_shillings = dollar * 119
+
+        print(f'{kenya_shillings:,} shillings')
+
+    @staticmethod
+    def euro_shillings():
+        euro = float(input('Enter Amount: '))
+        shilling = euro * 104.4
+        print(f'{shilling:,} shillings')
+
+
+c = Currency()
+c.pound_to_shillings()
+
+
+
